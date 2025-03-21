6340a3cbb9e679cd8d6a38951b3749970d79058c
aab0030 <alamillabuenoangel@gmail.com>
2025-03-21 10:35:55 +0100

    Proyecto con rutas añadidas

--- 5-user-app/src/app/components/user-app.component.ts
+++ 5-user-app/src/app/components/user-app.component.ts
@@ -1,76 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
-import { UserComponent } from "./user/user.component";
-import { UserFormComponent } from './user-form/user-form.component';
 import Swal from 'sweetalert2';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SharingDataService } from '../services/sharing-data.service';
 
 @Component({
   selector: 'user-app',
-  imports: [UserComponent, UserFormComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './user-app.component.html'
 })
 export class UserAppComponent implements OnInit{
-  title: string = 'Listado de usuarios';
 
   users: User[] = [];
 
-  userSelected: User;
 
-  open: boolean = false;
-
-  constructor(private service: UserService) {
-    this.userSelected = new User;
+  constructor(
+    private router: Router,
+    private service: UserService,
+    private sharingData: SharingDataService
+  ) {
   }
   ngOnInit(): void {
     this.service.findAll().subscribe( users => this.users = users);
+    //Para suscribirnos a todos los eventos 
+    this.addUser();
+    this.removeUser();
   }
 
-  addUser(user: User){
-    if (user.id > 0) {
-      this.users = this.users.map( u => (u.id == user.id) ? {... user} : u)
-    } else {
-      this.users = [... this.users, {... user, id: new Date().getTime()}];
-    }
-    Swal.fire({
-      title: "Guardado!",
-      text: "Usuario guardado correctamente!",
-      icon: "success"
-    });
-    this.userSelected = new User();
-  }
-
-  removeUser(id: number): void {
-    Swal.fire({
-      title: "Seguro que quieres borrarlo?",
-      text: "Esta acción es permanente!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Sí!",
-      cancelButtonText: "No!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-
-        this.users = this.users.filter((user) => user.id != id);
-
-        Swal.fire({
-          title: "Eliminado!",
-          text: "El usuario ha sido borrado de la base de datos.",
-          icon: "success"
-        });
+  addUser(){
+    this.sharingData.newUserEventEmitter.subscribe(user => {
+      if (user.id > 0) {
+        this.users = this.users.map( u => (u.id == user.id) ? {... user} : u)
+      } else {
+        this.users = [... this.users, {... user, id: new Date().getTime()}];
       }
-    });
-
+      //Redundante?, cuando crea un usuario con el form lo rediige a la pagina de usuarios, pasandole la lista de usuarios actualizada
+      this.router.navigate(['/users'], {state: { users: this.users}});
+      Swal.fire({
+        title: "Guardado!",
+        text: "Usuario guardado correctamente!",
+        icon: "success"
+      });
+    })
   }
 
-  setSelectedUser(user: User): void {
-    this.userSelected = {... user};
-    this.open = true;
+  removeUser(): void {
+    this.sharingData.idUserEventEmitter.subscribe(id => {
+      Swal.fire({
+        title: "Seguro que quieres borrarlo?",
+        text: "Esta acción es permanente!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí!",
+        cancelButtonText: "No!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+  
+          this.users = this.users.filter((user) => user.id != id);
+          
+          this.router.navigate(['/users/create'], {skipLocationChange: true}).then(() =>{
+            this.router.navigate(['/users'], {state: { users: this.users}});
+          })
+  
+          Swal.fire({
+            title: "Eliminado!",
+            text: "El usuario ha sido borrado de la base de datos.",
+            icon: "success"
+          });
+        }
+      });
+    })
   }
 
-  setOpen() {
-    this.open = !this.open;
-  }
+  
 }
--- 5-user-app/src/app/components/user-form/user-form.component.ts
+++ 5-user-app/src/app/components/user-form/user-form.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../../models/user';
+import { SharingDataService } from '../../services/sharing-data.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'user-form',
@@ -9,32 +11,30 @@ import { User } from '../../models/user';
 })
 export class UserFormComponent {
   
-  @Input() user: User;
+  user: User;
 
-  @Output() openEventEmitter = new EventEmitter();
+  constructor(
+    private router: Router,
+    private sharingData: SharingDataService) {{
 
-  @Output() newUserEventEmitter: EventEmitter<User> = new EventEmitter();
-
-  constructor() {{
-    this.user = new User();
+    if(this.router.getCurrentNavigation()?.extras.state) {
+      this.user = this.router.getCurrentNavigation()?.extras.state!['user'];
+    } else {
+      this.user = new User();
+    }
   }}
 
   onSubmit(userform: NgForm): void {
     if(userform.valid) {
-      this.newUserEventEmitter.emit(this.user);
+      this.sharingData.newUserEventEmitter.emit(this.user);
       console.log(this.user);
     }
     userform.reset();
     userform.resetForm();
-    this.onOpenClose();
   }
   onClear(userform: NgForm): void{
     this.user = new User();
     userform.reset();
     userform.resetForm();
-  }
-
-  onOpenClose(): void {
-    this.openEventEmitter.emit();
   }
 }
